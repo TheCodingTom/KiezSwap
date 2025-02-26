d266522d7fcec160303f3ecb777ef820cf54f03f
TheCodingTom <dastolto.t@gmail.com>
2025-02-26 14:52:08 +0100

    image upload via client works

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,16 +1,60 @@
 import { TextField } from "@mui/material";
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 function Register() {
+  const [selectedFile, setSelectedFile] = useState<File | string>("");
+
+  const handleAttachFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e);
+    const file = e.target.files?.[0];
+
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleImageUpload = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formdata = new FormData();
+    formdata.append("image", selectedFile);
+
+    const requestOptions = {
+      method: "POST",
+      body: formdata,
+    
+    };
+
+
+    try {
+      const response = await fetch("http://localhost:4000/api/users/uploadImage", requestOptions)
+      const result = await response.json()
+      console.log('result :>> ', result);
+      
+    } catch (error) {
+      console.log('error :>> ', error);
+    }
+
+    
+  };
+
   return (
     <div className="form-container">
       <h1>Register</h1>
-      <form action="submit" className="register-form">
+      <form action="submit" className="register-form" onSubmit={handleImageUpload}>
         <TextField id="outlined-basic" label="Email" variant="outlined" />
         <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <input
+          type="file"
+          name="image"
+          id="image"
+          accept="image/*"
+          onChange={handleAttachFile}
+        />
+        <button >Upload image</button>
         <Button>Register</Button>
       </form>
-      
     </div>
   );
 }
--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -5,7 +5,10 @@ import cloudinaryUpload from "../utilities/cloudinaryUpload.js";
 const getAllUsers = async (req, res) => {
   //   console.log("all users working");
   try {
-    const allUsers = await UserModel.find().populate({path:"listings", select:["name","location","likes"]})
+    const allUsers = await UserModel.find().populate({
+      path: "listings",
+      select: ["name", "location", "likes"],
+    });
     console.log(allUsers);
     if (allUsers.length === 0) {
       // try to cover as much responses as possible to build a proper UI
@@ -28,24 +31,33 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-const imageUpload = async (req,res) => {
+const imageUpload = async (req, res) => {
   console.log("image upload working");
-  console.log('req.file :>> ', req.file);
+  console.log("req.file :>> ", req.file);
 
   if (!req.file) {
-    return res.status(500).json (
-      {error: "file not supported"}
-    )
+    return res.status(500).json({ error: "file not supported" });
   }
 
   if (req.file) {
     // check file size here or do it in multer.js with fileSize
     // if we have a req.file we upload it to Cloudinary
 
-    const uploadedImage = await cloudinaryUpload(req.file)
-    console.log("image uploaded", uploadedImage);
+    const uploadedImage = await cloudinaryUpload(req.file);
 
+    if (!uploadedImage) {
+      return res.status(400).json({
+        error:"Image couldn't be uploaded"
+      })
+    }
+    if (uploadedImage) {
+      return res.status(200).json({
+        message: "Image uploaded successfully",
+        imageURL: uploadedImage.secure_url,
+      });
+    }
+    console.log("image uploaded", uploadedImage);
   }
-}
+};
 
 export { getAllUsers, imageUpload };
