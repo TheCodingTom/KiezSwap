9fd161a98a24fc983721601a8b4101ad85b62b82
TheCodingTom <dastolto.t@gmail.com>
2025-02-27 11:27:33 +0100

    user registration via client working

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,13 @@
 import { TextField } from "@mui/material";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
-import { ImageUploadOkResponse, UserType } from "../types/customTypes";
+import { ImageUploadOkResponse, RegisterOkResponse, UserRegisterFormType, UserType } from "../types/customTypes";
 
 function Register() {
   const [selectedFile, setSelectedFile] = useState<File | string>("");
-  const [newUser, setNewUser] = useState<UserType | null>(null);
+  const [newUser, setNewUser] = useState<UserRegisterFormType | null>(null);
+
+  const [user, setUser] = useState<UserType | null>(null) // this keeps the user logged in
 
   const handleAttachFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e);
@@ -51,9 +53,43 @@ function Register() {
     setNewUser({ ...newUser!, [e.target.name]: e.target.value });
   };
 
-  const handleRegisterSubmit = (e:  React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleRegisterSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
     console.log(newUser);
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    const urlencoded = new URLSearchParams();
+    // Input validation here - username,password,email
+    if (newUser) {
+      urlencoded.append("username", newUser.username);
+      urlencoded.append("email", newUser.email);
+      urlencoded.append("password", newUser.password);
+    } else {
+      console.log("All the fields are required");
+    }
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:4000/api/users/register",
+        requestOptions
+      );
+      const result = await response.json() as RegisterOkResponse
+      console.log(result.message);
+      setUser(result.user)
+      
+
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
   };
 
   return (
@@ -77,7 +113,7 @@ function Register() {
       </div>
 
       <div className="form-container">
-        <form className="register-form" >
+        <form className="register-form">
           <TextField
             id="outlined-basic"
             label="Username"
@@ -99,18 +135,22 @@ function Register() {
             name="password"
             onChange={handleRegisterInputChange}
           />
+
           <Button onClick={handleRegisterSubmit}>Register</Button>
         </form>
       </div>
 
       <div>
-        {newUser?.image && (
-          <img
-            src={newUser.image}
+        {user && 
+         <div>
+          <h3>{user.username}</h3>
+           <img
+            src={user.image}
             alt="avatar image"
             style={{ width: "200px" }}
           />
-        )}
+         </div>
+        }
       </div>
     </>
   );
--- client/src/types/customTypes.ts
+++ client/src/types/customTypes.ts
@@ -1,25 +1,37 @@
 export type UserType = {
-  username: string;
   email: string;
-  password: string;
+  id: string;
   image: string;
-}
+  username: string;
+};
+
+export type UserRegisterFormType = {
+  email: string;
+  image: string;
+  password: string;
+  username: string;
+};
 
 export type ImageUploadOkResponse = {
   message: string;
   imageURL: string;
-}
+};
+
+export type RegisterOkResponse = {
+  message: string;
+  user: UserType;
+};
 
 export type ListingType = {
-    _id: string;
-    name: string;
-    description: string;
-    location: LocationType;
-    category: string;
-    likes: number
-  };
+  _id: string;
+  name: string;
+  description: string;
+  location: LocationType;
+  category: string;
+  likes: number;
+};
 
-  export type LocationType = {
-    city: string;
-    district: string;
-  }
+export type LocationType = {
+  city: string;
+  district: string;
+};
