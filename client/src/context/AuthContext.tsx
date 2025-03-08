01423bd44df27ebb1dd8fa6ef90df5a89431e83f
TheCodingTom <dastolto.t@gmail.com>
2025-03-08 19:08:24 +0100

    user stays logged in after refreshing

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -4,6 +4,7 @@ type AuthContextProviderProps = {
 
 type AuthContextType = {
   user: User | null;
+  isAuthenticated: boolean;
   register: (
     username: string,
     email: string,
@@ -11,12 +12,13 @@ type AuthContextType = {
   ) => Promise<void>;
 
   login: (email: string, password: string) => Promise<void>;
-  logout:() => void
+  logout: () => void;
+  checkUserStatus: () => void;
 };
-
 
 const contextInitialValue: AuthContextType = {
   user: null,
+  isAuthenticated: false,
   register: () => {
     throw Error("context not initialised");
   },
@@ -26,20 +28,24 @@ const contextInitialValue: AuthContextType = {
   logout: () => {
     throw Error("context not initialised");
   },
+  checkUserStatus: async () => {},
 };
 
-
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { LoginOkResponse, RegisterOkResponse, User } from "../types/customTypes";
+import {
+  LoginOkResponse,
+  RegisterOkResponse,
+  User,
+} from "../types/customTypes";
 
 
 export const AuthContext = createContext<AuthContextType>(contextInitialValue);
 
-
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-
   const [user, setUser] = useState<User | null>(null);
- 
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const token = localStorage.getItem("token");
 
   const register = async (
     username: string,
@@ -71,8 +77,6 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
       );
       const result = (await response.json()) as RegisterOkResponse;
       console.log(result.message);
-      
-      
     } catch (error) {
       console.log("error :>> ", error);
     }
@@ -102,9 +106,8 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
         requestOptions
       );
       const result = (await response.json()) as LoginOkResponse;
-      alert(result.message)
+      alert(result.message);
       console.log(result);
-      setUser(result.user);
 
       if (!result.token) {
         // do something if token is not there
@@ -112,41 +115,111 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
 
       if (result.token) {
         // store token in the local storage
-        localStorage.setItem("token", result.token)
+        localStorage.setItem("token", result.token);
+        setUser(result.user);
+        setIsAuthenticated(true);
       }
-      
-      
     } catch (error) {
       console.log("error :>> ", error);
     }
   };
 
   const logout = () => {
-    localStorage.removeItem("token")
-    setUser(null)
+    localStorage.removeItem("token");
+    setUser(null);
     console.log("user logged out successfully");
-  }
+  };
 
+
+  const checkUserStatus = async () => {
+    if (token) {
+      setIsAuthenticated(true);
+
+      if (!user) {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+
+        const requestOptions = {
+          method: "GET",
+          headers: myHeaders,
+        };
+
+        try {
+          const response = await fetch(
+            "http://localhost:4000/api/users/profile",
+            requestOptions
+          );
+          if (!response.ok) {
+            console.log("Log in again, redirect user to login page");
+            return;
+          }
+
+          if (response.ok) {
+            const result = await response.json();
+            console.log('result in the check user status):>> ', result);
+            setUser(result);
+            
+          }
+        } catch (error) {
+          console.log("error :>> ", error);
+        }
+      }
+    } else {
+      setIsAuthenticated(false);
+    }
+  };
 
   useEffect(() => {
-    const token = localStorage.getItem("token")
-    if (token) {
-      console.log("user is logged in");
-    }
-    else {
-      console.log("user is logged out");
-    }
-  }, [])
-  
+    checkUserStatus();
+  }, []);
+
+
+  // const { token, userStatusMessage } = useUserStatus();
+
+  // const checkUserStatus = async () => {
+  //   const myHeaders = new Headers();
+  //     myHeaders.append("Authorization", `Bearer ${token}`);
+
+  //     const requestOptions = {
+  //       method: "GET",
+  //       headers: myHeaders,
+  //     };
+
+  //     try  {
+  //       const response = await fetch(
+  //       "http://localhost:4000/api/users/profile",
+  //       requestOptions
+  //     );
+
+  //     // if response ok...
+
+  //     const result = await response.json();
+  //     console.log(result);
+  //     setUser(result.user)
+
+  //     console.log(userStatusMessage);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  // }
+
+  // useEffect runs on mount so checkUserStatus can run only if there's a token
+  // useEffect(() => {
+  //   if (token) {
+  //     checkUserStatus()
+  //   }
+  // }, [token])
 
   return (
     <div>
       <AuthContext.Provider
         value={{
           user,
+          isAuthenticated,
           register,
           login,
-          logout
+          logout,
+          checkUserStatus,
         }}
       >
         {children}
