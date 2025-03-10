a7a3b8b76c47b38a051ab3ec19d173567f7fe3da
TheCodingTom <dastolto.t@gmail.com>
2025-03-10 14:32:26 +0100

    uploading image inside new listing function in frontend

--- client/src/pages/NewListing.tsx
+++ client/src/pages/NewListing.tsx
@@ -1,6 +1,6 @@
-import { TextField } from "@mui/material";
+
 import { useContext, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 function NewListing() {
@@ -12,16 +12,32 @@ function NewListing() {
     district: "",
   });
 
+   const [selectedFile, setSelectedFile] = useState<File | string>("");
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+    const handleAttachFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e);
+        const file = e.target.files?.[0];
+    
+        if (file instanceof File) {
+          // if our document/image matches the File type the function will run
+          setSelectedFile(file);
+          setImagePreview(URL.createObjectURL(file));
+        }
+      };
+
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleFormSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleFormSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
     e.preventDefault();
 
-    const token = localStorage.getItem("token"); 
+    const token = localStorage.getItem("token");
     if (!token) {
       console.error("No auth token found");
       return;
@@ -33,6 +49,7 @@ function NewListing() {
       form.append("description", formData.description);
       form.append("district", formData.district);
       form.append("user", user.id);
+      form.append("image", selectedFile);
       // form.append("image", selectedFile);
 
       try {
@@ -61,43 +78,56 @@ function NewListing() {
       <h1>Add new listing</h1>
 
       <div className="form-container">
-        <form className="register-form" >
-          <TextField
-            label="Name"
-            variant="outlined"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="Description"
-            variant="outlined"
-            name="description"
-            value={formData.description}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="District"
-            variant="outlined"
-            name="district"
-            value={formData.district}
-            onChange={handleInputChange}
-          />
+        <form className="register-form">
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
 
-          {/* <input
+          <Form.Group controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Enter description"
+              name="description"
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="district">
+            <Form.Label>District</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter district"
+              name="district"
+              value={formData.district}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+
+          <input
             type="file"
             name="image"
             id="image"
             accept="image/*"
             onChange={handleAttachFile}
           />
+        
           {imagePreview && (
             <img
               src={imagePreview}
-              alt="preview"
-              style={{ width: "100px", marginTop: "10px" }}
+              alt="preview of avatar image"
+              style={{ width: "100px" }}
             />
-          )} */}
+          )}
         </form>
       </div>
       <Button onClick={handleFormSubmit}>Submit</Button>
