65779eacd4c938681955add3501ef2de8414a561
TheCodingTom <dastolto.t@gmail.com>
2025-03-10 17:31:04 +0100

    added home page animation - true focus

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import TrueFocus from "../components/TrueFocus";
 
 function Home() {
-
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <div>
-      {user ? <h1>Welcome to KiezSwap, {user?.username}!</h1> : <h1>Welcome to KiezSwap!</h1>}
+      {user ? (
+        <h1>Welcome to KiezSwap, {user?.username}!</h1>
+      ) : (
+        <h1>Welcome to KiezSwap!</h1>
+      )}
+
+      <TrueFocus
+        sentence="Kiez Swap"
+        manualMode={false}
+        blurAmount={5}
+        borderColor="blue"
+        animationDuration={0.5}
+        pauseBetweenAnimations={1}
+      />
     </div>
   );
 }
