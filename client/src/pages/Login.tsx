a78272399cd5232809318751010f6b584ca24be9
TheCodingTom <dastolto.t@gmail.com>
2025-03-10 17:19:00 +0100

    added input validation in login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,6 @@
-
 import React, { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
-import { LoginCredentials } from "../types/customTypes";
 import { Link, useNavigate } from "react-router";
 
 function Login() {
@@ -10,31 +8,65 @@ function Login() {
 
   const goToHome = useNavigate();
 
-  const [loginCredentials, setLoginCredentials] =
-    useState<LoginCredentials | null>(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLoginInputChange = (
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // regular expression used to validate emails
+    return emailRegex.test(email); // test method checks if the email string matches the pattern
+  };
+
+  const validatePassword = (password: string) => {
+    return password.length >= 6;
+  };
+
+  const handleEmailInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
-    setLoginCredentials({
-      ...loginCredentials!,
-      [e.target.name]: e.target.value,
-    });
+    setEmail(e.target.value);
+    setErrors({
+      ...errors,
 
-    // input validation
+      email: validateEmail(e.target.value) ? "" : "Invalid email format",
+    });
+  };
+
+  const handlePasswordInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setPassword(e.target.value);
+    setErrors({
+      ...errors,
+
+      password: validatePassword(e.target.value)
+        ? ""
+        : "Password must be at least 6 characters",
+    });
   };
 
   const handleSubmitLogin = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
-    if (loginCredentials)
-      login(loginCredentials.email, loginCredentials.password);
-    goToHome("/");
+    if (!validateEmail(email) || !validatePassword(password)) {
+      setErrors({
+        email: validateEmail(email) ? "" : "Invalid email format",
+        password: validatePassword(password)
+          ? ""
+          : "Password must be at least 6 characters",
+      });
+      return;
+    }
 
-    // input validation
+    if (email && password) {
+      login(email, password);
+      goToHome("/");
+    }
   };
 
   return (
@@ -43,37 +75,41 @@ function Login() {
 
       <div className="form-container">
         <form className="register-form">
-       
-
           <Form.Group controlId="email">
             <Form.Control
               type="text"
               placeholder="Enter email"
               name="email"
-              onChange={handleLoginInputChange}
-              
+              onChange={handleEmailInputChange}
             />
           </Form.Group>
 
+          {errors.email && errors.email.length > 0 ? (
+            <p className="error">{errors.email}</p>
+          ) : (
+            ""
+          )}
 
           <Form.Group controlId="password">
             <Form.Control
               type="text"
               placeholder="Enter password"
               name="password"
-              onChange={handleLoginInputChange}
-              
+              onChange={handlePasswordInputChange}
             />
           </Form.Group>
 
+          {errors.password && errors.password.length > 0 ? (
+            <p className="error">{errors.password}</p>
+          ) : (
+            ""
+          )}
         </form>
-        <div>
-          <Button onClick={handleSubmitLogin}>Login</Button>
-          <p>
-            Don't have an account yet?{" "}
-            <Link to="/register">Create one here!</Link>
-          </p>
-        </div>
+        {!errors.email && !errors.password ?  <Button onClick={handleSubmitLogin}>Login</Button> : <Button disabled >Login</Button>}
+        <p>
+          Don't have an account yet?{" "}
+          <Link to="/register">Create one here!</Link>
+        </p>
       </div>
 
       <div>
