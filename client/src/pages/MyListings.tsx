5ff69d400509d1dc91f0a0093f788ff761b0858d
TheCodingTom <dastolto.t@gmail.com>
2025-03-14 10:57:02 +0100

    fetching user's listings works

--- client/src/pages/MyListings.tsx
+++ client/src/pages/MyListings.tsx
@@ -1,4 +1,29 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 function MyListings() {
+  const { user } = useContext(AuthContext);
+
+  const userId = user?.id;
+
+  const getUserListings = async () => {
+    const requestOptions = {
+      method: "GET",
+    };
+
+    const response = await fetch(
+      `http://localhost:4000/api/listings/all?userId=${userId}`,
+      requestOptions
+    );
+
+    const result = await response.json();
+    console.log(result);
+  };
+
+  useEffect(() => {
+    getUserListings();
+  }, []);
+
   return (
     <div>
       <h1>My listings page</h1>
