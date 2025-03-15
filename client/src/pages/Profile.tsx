121d0b057703ff138e2b817422fb0dc65165401f
TheCodingTom <dastolto.t@gmail.com>
2025-03-15 09:12:28 +0100

    installed lineicons and created Icons component

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,27 +2,13 @@ import { useContext } from "react";
 
 import { AuthContext } from "../context/AuthContext";
 import UploadAvatar from "../components/UploadAvatar";
-import GooeyNav from "../components/GooeyNavItem";
 
 function Profile() {
   const { user } = useContext(AuthContext);
   console.log(user);
-  const items = [{ label: "Home" }, { label: "About" }, { label: "Contact" }];
 
   return (
     <div>
-      <div style={{ height: "600px", position: "relative" }}>
-        <GooeyNav
-          items={items}
-          animationTime={600}
-          pCount={15}
-          minDistance={20}
-          maxDistance={42}
-          maxRotate={75}
-          colors={[1, 2, 3, 1, 2, 3, 1, 4]}
-          timeVariance={300}
-        />
-      </div>
       <div className="profile-container">
         {user && (
           <div className="profile-container">
