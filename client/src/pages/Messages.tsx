6c808d794f1bebf53cf84dfd823c08a9a1e7e1c2
TheCodingTom <dastolto.t@gmail.com>
2025-03-17 15:52:20 +0100

    fetching chats works (frontend)

--- client/src/pages/Messages.tsx
+++ client/src/pages/Messages.tsx
@@ -1,4 +1,33 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 function Messages() {
+  const { user } = useContext(AuthContext);
+
+  const getChats = async () => {
+    const requestOptions = {
+      method: "GET",
+    };
+
+    const response = await fetch(
+      `http://localhost:4000/api/chats/userChats/?sellerId=${user?.id}&buyerId=${user?.id}`,
+      requestOptions
+    );
+
+    try {
+      if (response) {
+        const result = await response.json();
+        console.log(result);
+      }
+    } catch (error) {
+      console.log("error :>> ", error);
+    }
+  };
+
+  useEffect(() => {
+    getChats();
+  }, []);
+
   return (
     <div>
       <h1>this is the messages page</h1>
