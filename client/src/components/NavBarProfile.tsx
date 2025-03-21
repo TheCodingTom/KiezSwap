48494795badf223be064be02cba5cf9e172f2382
TheCodingTom <dastolto.t@gmail.com>
2025-03-21 13:26:35 +0100

    showing navbarprofile only if user is logged in

--- client/src/components/NavBarProfile.tsx
+++ client/src/components/NavBarProfile.tsx
@@ -1,25 +1,34 @@
 import { Nav } from "react-bootstrap";
 import { NavLink } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 function NavBarProfile() {
+  const { user } = useContext(AuthContext);
   return (
-    <Nav className="justify-content-center">
-      <Nav.Item className="shake-left-right">
-        <Nav.Link as={NavLink} to={"/profile/userlistings"}>
-          My Listings
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item className="shake-left-right">
-        <Nav.Link as={NavLink} to={"/profile/messages"}>
-          Messages
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item className="shake-left-right">
-        <Nav.Link as={NavLink} to={"/profile/favourites"}>
-          Favourites
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <div>
+      {user ? (
+        <Nav className="justify-content-center">
+          <Nav.Item className="shake-left-right">
+            <Nav.Link as={NavLink} to={"/profile/userlistings"}>
+              My Listings
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="shake-left-right">
+            <Nav.Link as={NavLink} to={"/profile/messages"}>
+              Messages
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="shake-left-right">
+            <Nav.Link as={NavLink} to={"/profile/favourites"}>
+              Favourites
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
 
