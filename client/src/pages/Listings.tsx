f4f96195a6b490136467e45c447f7af9d122d8c4
TheCodingTom <dastolto.t@gmail.com>
2025-03-23 16:01:46 +0100

    created listings css file

--- client/src/pages/Listings.tsx
+++ client/src/pages/Listings.tsx
@@ -4,15 +4,21 @@ import Grid from "../components/Grid";
 import { Button } from "react-bootstrap";
 import { NavLink } from "react-router";
 import { AuthContext } from "../context/AuthContext";
+import "../styles/Listings.css";
 
 function Listings() {
-  
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
   return (
     <>
       <div className="button-container">
-      {user ? <NavLink to={"/newlisting"}><Button>Add new listing</Button></NavLink> : ""}
+        {user ? (
+          <NavLink to={"/newlisting"}>
+            <Button>Add new listing</Button>
+          </NavLink>
+        ) : (
+          ""
+        )}
       </div>
       <Grid />
     </>
