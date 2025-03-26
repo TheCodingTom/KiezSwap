f0863aa700acd1e6d940165e34c76969e4dd0483
TheCodingTom <dastolto.t@gmail.com>
2025-03-26 15:22:43 +0100

    added cors options to accept request only from specified origins

--- server/index.js
+++ server/index.js
@@ -25,6 +25,27 @@ const addMiddlewares = () => {
     })
   );
   app.use(cors());
+  // Using CORS options to secure the origin of the requests
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://kiezswapclient.vercel.app",
+    "http://kiezswapclient.vercel.app",
+  ];
+  const corsOptions = {
+    origin: function (origin, callback) {
+      //! origin will allow to accept direct calls to the api , with no heading, e.g. http://localhost:5001/api/cities/all
+      // origin will allow requests with no header (origin===undefined), the direct ones (using directly the server url). This solution will now accept only request from those 2 origins, or with no header.
+      //Accepting requests with no header might pose a security threat ...research how convinient the solution is.
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        // if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+  app.use(cors(corsOptions));
 
   cloudinaryConfig();
 
