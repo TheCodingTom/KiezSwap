ff7792d8b19b477f2e2ba3272239158434e6fa6b
TheCodingTom <dastolto.t@gmail.com>
2025-03-26 15:43:34 +0100

    manifest included for PWA settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  manifest: {
+    name: "Kiezswap app",
+    short_name: "PR MERN",
+    description:
+      "This app was created as a MERN project at CAB from Thomas D'Astolto.",
+    icons: [
+      {
+        src: "assets/maskable_icon_x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "assets/maskable_icon_x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#000000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     port: 5173,
   },
